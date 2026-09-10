8b594c15d6b56ade150d88d60b64430479f61936
NehaDhikale021 <nehadhikale1@gmail.com>
2026-09-11 02:32:37 +0530

    Secure database credentials with environment variable

--- src/com/foodlink/DBConnection.java
+++ src/com/foodlink/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/foodlink";
     private static final String USER = "root";
-    private static final String PASSWORD = "Neha@789";
+    private static final String PASSWORD = System.getenv("FOODLINK_DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
